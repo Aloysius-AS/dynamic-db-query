ba5b3fcbc38bb810e50bedb8236a5d5ff38a8d93
Aloysius <63091482+Aloysius-AS@users.noreply.github.com>
2020-04-20 12:14:28 +0800

    Refactor API JSON input

--- routes/testApi.js
+++ routes/testApi.js
@@ -3,6 +3,7 @@ const logger = require('../logger');
 const connectionPool = require('../database/connectionPool');
 const dbQueryBuilder = require('../database/queryBuilder');
 const { APIErrorHandler } = require('../helpers/apiErrorHandler');
+const validateApiJsonInput = require('../helpers/jsonApiInputValidator');
 
 router.route('/circleData').get((req, res) => {
 	try {
@@ -32,64 +33,7 @@ router.route('/circleData').get((req, res) => {
 
 router.route('/pdfImage').get((req, res, next) => {
 	const { schema_name, base_table_name, columns, join, filter } = req.body;
-
-	// TODO: Test robustness of log. Test that errors are logged.
-
-	/////////// Start: Validation of JSON input
-	// TODO: Validate the input JSON. Extract in separate file
-	// validate is proper JSON
-	// validate that fields are provided
-	// validate whether the value should be string, array of string/json, digit etc
-
-	let clientApiRequestErrorMessage = [];
-
-	if (!schema_name) {
-		clientApiRequestErrorMessage.push('Database schema must be provided.');
-	}
-	if (!base_table_name) {
-		clientApiRequestErrorMessage.push('Database table must be provided.');
-	}
-
-	if (join) {
-		join.forEach((joinObj) => {
-			let acceptableJoinTypes = ['inner join', 'left join', 'right join'];
-			if (acceptableJoinTypes.indexOf(joinObj.join_type) == -1) {
-				clientApiRequestErrorMessage.push(
-					`Join type \'${joinObj.join_type}\' is not a valid input. Acceptable values are: ${acceptableJoinTypes}`
-				);
-			}
-		});
-	}
-
-	if (filter) {
-		filter.forEach((filterObj) => {
-			let acceptableFilterTypes = ['=', '!=', '>', '>=', '<', '<='];
-			if (acceptableFilterTypes.indexOf(filterObj.operator) == -1) {
-				clientApiRequestErrorMessage.push(
-					`Filter operator \'${filterObj.operator}\' is not a valid input. Acceptable values are: ${acceptableFilterTypes}`
-				);
-			}
-		});
-	}
-
-	if (clientApiRequestErrorMessage.length >= 1) {
-		let jsonErrorMsg = {
-			errorType: 'Invalid API request option',
-			errorMessage: clientApiRequestErrorMessage,
-		};
-
-		logger.info(
-			jsonErrorMsg,
-			`Invalid JSON input provided at route /pdfImage. The JSON validation errors are `
-		);
-
-		throw new APIErrorHandler(
-			400,
-			'Invalid API request option',
-			clientApiRequestErrorMessage
-		);
-	}
-	/////////// End: Validation of JSON input
+	validateApiJsonInput(schema_name, base_table_name, columns, join, filter);
 
 	/////////// Start: Query db
 	// TODO: Extract into DAL folder, returning the query promise
