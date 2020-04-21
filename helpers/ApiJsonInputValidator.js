127949c5f234b36c87407963ad62f4a414bc86e7
Aloysius <63091482+Aloysius-AS@users.noreply.github.com>
2020-04-21 22:15:43 +0800

    Update ApiJsonInputValidator.js
    Removal of redundant code

--- helpers/ApiJsonInputValidator.js
+++ helpers/ApiJsonInputValidator.js
@@ -20,7 +20,6 @@ class ApiJsonInputValidator {
 	}
 
 	validate() {
-		// TODO: Check that input is a JSON
 		const validateResult = apiInputValidationSchema.validate({
 			schema_name: this.schema_name,
 			base_table_name: this.base_table_name,
@@ -31,17 +30,6 @@ class ApiJsonInputValidator {
 
 		if (validateResult.error) {
 			this.generateValidationErrorMessage(validateResult.error.details);
-		}
-	}
-
-	validate_depreciated() {
-		this.isSchemaProvided();
-		this.isDatabaseTableProvided();
-		this.areJoinInputsValid();
-		this.areFilterInputsValid();
-
-		if (this.clientApiRequestErrorMessage.length >= 1) {
-			this.generateValidationErrorMessage();
 		}
 	}
 
@@ -84,78 +72,9 @@ class ApiJsonInputValidator {
 			this.clientApiRequestErrorMessage
 		);
 	}
-
-	isSchemaProvided() {
-		if (!this.schema_name) {
-			this.clientApiRequestErrorMessage.push(
-				'Database schema must be provided.'
-			);
-		}
-	}
-
-	isDatabaseTableProvided() {
-		if (!this.base_table_name) {
-			this.clientApiRequestErrorMessage.push(
-				'Database table must be provided.'
-			);
-		}
-	}
-
-	areJoinInputsValid() {
-		if (this.join) {
-			this.join.forEach((joinObj) => {
-				let acceptableJoinTypes = ['inner join', 'left join', 'right join'];
-				if (acceptableJoinTypes.indexOf(joinObj.join_type) == -1) {
-					this.clientApiRequestErrorMessage.push(
-						`Join type \'${joinObj.join_type}\' is not a valid input. Acceptable values are: ${acceptableJoinTypes}`
-					);
-				}
-			});
-		}
-	}
-
-	areFilterInputsValid() {
-		if (this.filter) {
-			this.filter.forEach((filterObj) => {
-				let acceptableFilterTypes = [
-					'=',
-					'!=',
-					'>',
-					'>=',
-					'<',
-					'<=',
-					'contains',
-				];
-				if (acceptableFilterTypes.indexOf(filterObj.operator) == -1) {
-					this.clientApiRequestErrorMessage.push(
-						`Filter operator \'${filterObj.operator}\' is not a valid input. Acceptable values are: ${acceptableFilterTypes}`
-					);
-				}
-			});
-		}
-	}
 }
 
-// options is for Joi to return all validation errors instead of the 1st error
-const apiInputValidationSchema_depreciated = Joi.object()
-	.keys({
-		schema_name: Joi.string().trim().required().messages({
-			'string.base': `'schema_name' should be a type of 'text'`,
-			'any.required': `Database schema, 'schema_name', is required`,
-		}),
-		base_table_name: Joi.string().trim().required().messages({
-			'string.base': `'schema_name' should be a type of 'text'`,
-			'any.required': `Database table, 'base_table_name', is required`,
-		}),
-		columns: Joi.optional(),
-		join: Joi.array().items({
-			join_type: Joi.string()
-				.valid('inner join', 'left join', 'right join')
-				.required(),
-		}),
-	})
-	.options({ abortEarly: false });
-
+// abortEarly in options method is for Joi to return all validation errors instead of the 1st error
 const apiInputValidationSchema = Joi.object()
 	.keys({
 		schema_name: Joi.string().trim().required(),
