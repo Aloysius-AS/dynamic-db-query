1d986ead4392f94e30b57387d7a0a0863ae258b1
Aloysius <63091482+Aloysius-AS@users.noreply.github.com>
2020-05-27 02:24:03 +0800

    Add new test for F-Test and misc updates
    Misc updates:
    1. In the migration file, changed the column type of rwt_m in testing table, ftest, to float8.
    
    2. Correct the path of env file for debugging.

--- src/database/migrations/20200525150928_ftest.js
+++ src/database/migrations/20200525150928_ftest.js
@@ -2,7 +2,7 @@ exports.up = (knex, Promise) => {
 	return knex.schema.createTable('ftest', (table) => {
 		table.increments();
 		table.string('sex_re').notNullable();
-		table.decimal('rwt_m', 14, 9);
+		table.specificType('rwt_m', 'float8');
 		table.string('ef_group');
 	});
 };
--- test/services/StatisticalTestService.test.js
+++ test/services/StatisticalTestService.test.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const expect = require('chai').expect;
+const { jStat } = require('jstat');
+const ttest = require('ttest');
 
 const StatisticalTestService = require('../../src/services/StatisticalTestService');
 let knex = require('../../src//database/queryBuilder');
+const { TEST_TYPES, ALT_HYPOTHESIS_VALUES } = require('../../src/constants');
 
 describe('F-Test (Anova)', function () {
 	let statsTestServiceInstance = null;
@@ -63,7 +66,7 @@ describe('F-Test (Anova)', function () {
 
 			let statsTestServiceInstance = new StatisticalTestService(
 				'public',
-				'f-test',
+				TEST_TYPES.F_TEST,
 				null,
 				null,
 				null,
@@ -72,8 +75,8 @@ describe('F-Test (Anova)', function () {
 			);
 
 			let expectedResult = [
-				['0.439024389', '0.428571433'],
-				['0.340425521', '0.512820542'],
+				[0.439024389, 0.428571433],
+				[0.340425521, 0.512820542],
 			];
 
 			let dataParsed = null;
@@ -88,6 +91,84 @@ describe('F-Test (Anova)', function () {
 				});
 
 			expect(dataParsed).to.deep.equal(expectedResult);
+		});
+	});
+
+	describe('Calculation', function () {
+		it('Should have the same results compared to a 2-sample T-Test', async function () {
+			let dataset = [
+				//#region Init data
+				{
+					table: 'ftest',
+					column: 'rwt_m',
+					filter: [
+						{
+							column: 'sex_re',
+							operator: '=',
+							value: 'male',
+						},
+						{
+							column: 'ef_group',
+							operator: '=',
+							value: 'HFrEF',
+						},
+					],
+				},
+				{
+					table: 'ftest',
+					column: 'rwt_m',
+					filter: [
+						{
+							column: 'sex_re',
+							operator: '=',
+							value: 'female',
+						},
+						{
+							column: 'ef_group',
+							operator: '=',
+							value: 'HFpEF',
+						},
+					],
+				},
+				//#endregion
+			];
+
+			let statsTestServiceInstance = new StatisticalTestService(
+				'public',
+				TEST_TYPES.F_TEST,
+				null,
+				null,
+				null,
+				null,
+				dataset
+			);
+
+			let pValueFTest,
+				pValueTTest,
+				dataParsed = null;
+
+			let tTestOptions = {
+				mu: 0,
+				varEqual: true,
+				alpha: 0.05,
+				alternative: ALT_HYPOTHESIS_VALUES.NOT_EQUAL,
+			};
+
+			await statsTestServiceInstance
+				.retrieveData()
+				.then((dataRetrieved) => {
+					dataParsed = statsTestServiceInstance.parseData(dataRetrieved);
+					pValueFTest = jStat.anovaftest(...dataParsed);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+
+			const tTestObj = ttest(...dataParsed, tTestOptions);
+			pValueTTest = tTestObj.pValue();
+
+			// just need to compare up to 3 decimal places as p-values are usually good up to 3 decimal places
+			expect(pValueFTest.toFixed(3)).to.equal(pValueTTest.toFixed(3));
 		});
 	});
 });
