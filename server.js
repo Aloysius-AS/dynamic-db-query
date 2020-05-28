cd0f6387735cf303126beec5cd2167edab9c240a
Aloysius <63091482+Aloysius-AS@users.noreply.github.com>
2020-05-28 11:45:06 +0800

    Toggle between HTTP and HTTPS server

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const expressPino = require('express-pino-logger');
 const helmet = require('helmet');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
 const logger = require('./logger');
 const { handleAPIError } = require('./src/helpers/apiErrorHandler');
 const clientApiKeyValidation = require('./src/helpers/authUtils');
 
 const app = express();
-const port = process.env.PORT || 5000;
-
 const expressLogger = expressPino({ logger });
 
 app.use(helmet());
@@ -22,9 +23,31 @@ const datapointApiRouter = require('./src/api/datapoint');
 const vectorApiRouter = require('./src/api/vector');
 const statsTestApiRouter = require('./src/api/statisticalTest');
 
-app.listen(port, () => {
-	logger.info(`Server is running on port: ${port}`);
-});
+if (process.env.NON_SSL_ENABLED === 'true') {
+	const non_ssl_port = process.env.NON_SSL_PORT || 8080;
+	app.listen(non_ssl_port, () => {
+		logger.info(`HTTP Server is running on port: ${non_ssl_port}`);
+	});
+}
+
+if (process.env.SSL_ENABLED === 'true') {
+	const ssl_port = process.env.SSL_PORT;
+
+	// serve the API with signed certificate on SSL port
+	const keyRelativePath = process.env.SSL_KEY_RELATIVE_PATH;
+	const certRelativePath = process.env.SSL_CERT_RELATIVE_PATH;
+	const httpsServer = https.createServer(
+		{
+			key: fs.readFileSync(path.resolve(__dirname, keyRelativePath)),
+			cert: fs.readFileSync(path.resolve(__dirname, certRelativePath)),
+		},
+		app
+	);
+
+	httpsServer.listen(ssl_port, () => {
+		logger.info(`HTTPS Server is running on port: ${ssl_port}`);
+	});
+}
 
 app.use('/v1/testAPI', testApiRouter);
 app.use('/v1/datapoint', datapointApiRouter);
